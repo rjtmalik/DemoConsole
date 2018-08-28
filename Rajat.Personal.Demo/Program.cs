5c0c240363939af498fd241a2bc121a5e5e48e15
Rajat Malik <rjtmalik@gmail.com>
2018-08-28 22:47:39 +0530

    First Commit

---
+++ Rajat.Personal.Demo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Rajat.Personal.Demo.DAL;
+
+namespace Rajat.Personal.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            var messages = RequestUrl.Instance.GetMessages(100);
+            foreach (var msg in messages)
+            {
+                using (msg)
+                {
+                    var result = client.SendAsync(msg).Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var data = result.Content.ReadAsStringAsync().Result;
+                        ScrappedData.Instance.SaveToBlob(data);
+                    }
+                }
+            }
+        }
+    }
+}
